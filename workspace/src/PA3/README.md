dfb4dea65832f13f886475e5a27c1c63aa1ce952
Megan Liu <mgnliu20@gmail.com>
2025-05-02 11:27:55 -0400

    pa3 - reads ocupancy grid mesage correctly and sets its rf

---
+++ workspace/src/PA3/README.md
@@ -0,0 +1,64 @@
+# Assignment: PA3 - Path Planner
+
+#### Author: Megan Liu
+
+#### Class: COSC81.01 Principles of Robot Design and Programming
+
+#### Instructor: Alberto Quattrini Li
+
+#### Term: Spring 2025
+
+#### Description: This code implements shape drawing behavior for the ROSbot2 using map_server. It plans a path and executes it.
+
+## Requirements
+
+- ROS2 -- tested on ROS2 humble, but other versions may work.
+
+## How to run:
+
+1. [Terminal 1] run docker using `docker compose up`
+2. close rosbot-gazebo in docker desktop
+3. [Terminal 2] enter pa2 directory (with the map files) and run the lightweight simulator Stage (installation instructions in PA2 if needed)
+
+```docker compose exec ros bash
+ cd pa2
+ ros2 launch stage_ros2 stage.launch.py world:=/root/catkin_ws/src/pa3/maze
+enforce_prefixes:=false one_tf_tree:=true
+```
+
+4. [Terminal 3] in a new terminal, run start the map server
+
+```
+ros2 run nav2_map_server map_server --ros-args -p yaml_filename:=pa3/maze.yml
+```
+
+5. [Terminal 3.5] visualize map
+
+```
+ros2 run rviz2 rviz2
+```
+
+6. [Terminal 5,6] start the loading of the map and publish the tf map-odom
+   (install map_server first)
+
+```
+sudo apt update &&
+sudo apt install ros-humble-nav2-map-server
+```
+
+```
+ros2 run nav2_util lifecycle_bringup map_server
+ros2 run tf2_ros static_transform_publisher 2 2 0 0 0 0 map rosbot/odom
+```
+
+7. [Terminal 8] run PA3
+
+```
+python3 pa3-planning.py
+```
+
+8. [Terminal 9] send a fresh map message
+
+```
+ros2 service call /map_server/load_map nav2_msgs/srv/LoadMap "{ map_url: 'pa3/maze.yml' }"
+```
---
+++ workspace/src/PA3/pa3-planning.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+# Author: AQL
+# Date: 2025/03/31
+
+import numpy as np
+
+import tf_transformations
+
+import rclpy # module for ROS APIs
+from rclpy.node import Node
+# http://docs.ros.org/en/noetic/api/nav_msgs/html/msg/OccupancyGrid.html
+from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import PoseStamped
+
+NODE_NAME = "planner"
+
+MAP_TOPIC = "map"
+POSE_TOPIC = "pose"
+
+MAP_FRAME_ID = "map"
+
+USE_SIM_TIME = True
+
+class Grid:
+    def __init__(self, occupancy_grid_data, width, height, resolution):
+        self.grid = np.reshape(occupancy_grid_data, (height, width))
+        self.resolution = resolution
+
+    def cell_at(self, r, c):
+        return self.grid[r, c]
+
+class Plan(Node):
+    def __init__(self, map_frame_id=MAP_FRAME_ID, node_name=NODE_NAME, context=None):
+        super().__init__(node_name, context=context)
+
+        # Workaround not to use roslaunch
+        use_sim_time_param = rclpy.parameter.Parameter(
+            'use_sim_time',
+            rclpy.Parameter.Type.BOOL, 
+            USE_SIM_TIME
+        )
+        self.set_parameters([use_sim_time_param])
+
+        self.sub = self.create_subscription(OccupancyGrid, MAP_TOPIC, self.map_callback, 1)
+
+        self.pose_pub = self.create_publisher(PoseStamped, POSE_TOPIC, 1)
+
+        self.map = None # the variable containing the map.
+
+        self.map_frame_id = map_frame_id
+        self.occgrid_frame_id = None
+
+    def map_callback(self, msg):
+        print("MAP CALLBACK")
+        self.map = Grid(msg.data, msg.info.width, msg.info.height, msg.info.resolution)
+        self.occgrid_frame_id = msg.header.frame_id
+        print(self.map.cell_at(0,1))
+
+    def publish_pose(self):
+        """Example of publishing an arrow, without orientation."""
+        pose_msg = PoseStamped()
+        pose_msg.header.stamp = self.get_clock().now().to_msg()
+        pose_msg.header.frame_id = self.map_frame_id
+        
+        pose_msg.pose.position.x = 1.0
+        pose_msg.pose.position.y = 1.0
+        quaternion = tf_transformations.quaternion_from_euler(np.pi/2, 0, 0)
+        pose_msg.pose.orientation.x = quaternion[0]
+        pose_msg.pose.orientation.y = quaternion[1]
+        pose_msg.pose.orientation.z = quaternion[2]
+        pose_msg.pose.orientation.w = quaternion[3]
+
+        self.pose_pub.publish(pose_msg)
+
+    def plan(self, start, end, serach): 
+        pass 
+
+
+def main(args=None):
+    # 1st. initialization of node.
+    rclpy.init(args=args)
+
+    p = Plan()
+
+    # try:
+    while rclpy.ok():
+        p.publish_pose()
+
+        rclpy.spin_once(p)
+    # except KeyboardInterrupt:
+    #     print("SOMETHING")
+    #     pass
+
+    rclpy.shutdown()
+    
+
+
+if __name__ == "__main__":
+    main()
